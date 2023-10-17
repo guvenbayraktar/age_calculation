573903e005eb8bfa4ad96c2831523da2aae382fa
guvenbayraktar <101019912+guvenbayraktar@users.noreply.github.com>
2023-10-18 00:49:35 +0300

    yas hesaplama programı değişiklik

--- _age_calculation.py
+++ _age_calculation.py
@@ -1,24 +1,5 @@
 from datetime import datetime
-from datetime import timedelta
 
-"""
-birthday = input("Dogum tarihinizi Gun.Ay.Yil olacak şekilde giriniz.: ")
-birthday = datetime.strptime(birthday, "%d.%m.%Y")
-birthday = birthday.date()
-print(birthday)
-
-current_date = datetime.today()
-current_date = current_date.date()
-print(current_date)
-
-age = current_date - birthday
-
-year = age.days // 365.25
-month = age.days % 365.25 // 30.4375
-day = age.days % 365.25 % 30.4375
-
-print(f'Yasiniz: {int(year)} yil, {int(month)} ay, {int(day)} gün')
-"""
 
 def age_calculate():
 
